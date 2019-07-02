2148f290f201f735ebe8a37ceeef51c8f6b42112
ysz <1582945670@qq.com>
2019-07-02 23:34:10 +0800

    readme and express-demo

---
+++ 04/code/express-demo/app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+
+// 1. 创建 app
+var app = express()
+
+app.get('/', function (req, res) {
+    // res.write('hello ')
+    // res.write('world')
+    // res.end()
+    // res.end('hello world!')
+    res.send('hello world!!')
+})
+
+app.get('/login', function (req, res) {
+    // res.write('hello ')
+    // res.write('world')
+    // res.end()
+    // res.end('hello world!')
+    res.send('login page')
+})
+
+
+app.listen(3000, function () {
+    console.log(`express app is running...`)
+})
