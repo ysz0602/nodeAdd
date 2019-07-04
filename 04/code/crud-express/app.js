0c6df4238ac8b26ac9b9b21b0aea3a6acb7b6a5b
ysz <1582945670@qq.com>
2019-07-04 20:55:33 +0800

    router.js

--- 04/code/crud-express/app.js
+++ 04/code/crud-express/app.js
@@ -1,5 +1,18 @@
+/**
+ * app.js 入门模块
+ * 职责：
+ *   启动服务
+ *   做一些服务相关配置
+ *     模板引擎
+ *     body-parser 解析表单 post 请求体
+ *     提供静态资源服务
+ *   挂载路由
+ *   监听端口启动服务
+ * 
+ * **/
+
 var express = require('express')
-var fs = require('fs')
+var router = require('./router')
 
 var app = express()
 
@@ -8,22 +21,8 @@ app.use('/public/', express.static('./public/'))
 
 app.engine('html', require('express-art-template'))
 
-app.get('/', function (req, res) {
-    fs.readFile('./db.json', 'utf8', function(err, data) {
-        if (err) {
-            return res.status(500).send('Server error!')
-        }
-        // console.log(data)
-        res.render('index.html', {
-            fruits: [
-                '苹果',
-                '香蕉',
-                '橘子'
-            ],
-            students: JSON.parse(data).students
-        })
-    })
-})
+// 把路由容器挂载到 App 服务
+app.use(router)
 
 app.listen(3000, function (req,res) {
     console.log(`running 3000 ...`)
---
+++ 04/code/crud-express/router.js
@@ -0,0 +1,106 @@
+/**
+ * router.js 模块
+ * 职责：
+ *   处理路由
+ *   根据不同的请求方法+请求路径设置具体的请求处理函
+ * 模块职责要单一，不要乱写
+ */
+
+var fs = require('fs')
+
+// Express 提供了一种更好的方式
+// 专门用来包装路由的
+var express = require('express')
+
+// 1.创建一个路由容器
+var router = express.Router()
+
+// 2.把路由都挂载到 router 路由容器中
+router.get('/students', function (req, res) {
+    fs.readFile('./db.json', 'utf8', function(err, data) {
+        if (err) {
+            return res.status(500).send('Server error!')
+        }
+        // console.log(data)
+        res.render('index.html', {
+            fruits: [
+                '苹果',
+                '香蕉',
+                '橘子'
+            ],
+            students: JSON.parse(data).students
+        })
+    })
+})
+
+router.get('/students/new', function (req, res) {
+
+})
+
+router.get('/students/new', function (req, res) {
+    
+})
+
+router.get('/students/new', function (req, res) {
+    
+})
+
+router.get('/students/new', function (req, res) {
+    
+})
+
+router.get('/students/new', function (req, res) {
+    
+})
+
+router.get('/students/new', function (req, res) {
+    
+})
+
+// 3. 把 router 导出
+module.exports = router
+
+
+// 这样也不方便
+// module.exports = function (app) {
+//     app.get('/students', function (req, res) {
+//         fs.readFile('./db.json', 'utf8', function(err, data) {
+//             if (err) {
+//                 return res.status(500).send('Server error!')
+//             }
+//             // console.log(data)
+//             res.render('index.html', {
+//                 fruits: [
+//                     '苹果',
+//                     '香蕉',
+//                     '橘子'
+//                 ],
+//                 students: JSON.parse(data).students
+//             })
+//         })
+//     })
+    
+//     app.get('/students/new', function (req, res) {
+    
+//     })
+    
+//     app.get('/students/new', function (req, res) {
+        
+//     })
+    
+//     app.get('/students/new', function (req, res) {
+        
+//     })
+    
+//     app.get('/students/new', function (req, res) {
+        
+//     })
+    
+//     app.get('/students/new', function (req, res) {
+        
+//     })
+    
+//     app.get('/students/new', function (req, res) {
+        
+//     })
+// }
